f644f707907c34da0fdee718615971ab954db830
Chivalrybean <jlowmaster@gmail.com>
2021-07-06 22:14:49 -0700

    Added physical_effect()

---
+++ whiskerbot.py
@@ -0,0 +1,6 @@
+import random as rng
+
+spell_types = [()]
+
+def physical_effect():
+    return rng.choice(["Acid", "Amber", "Bark", "Blood", "Bone", "Brine", "Clay", "Crow", "Crystal", "Ember", "Flesh", "Fungus", "Glass", "Honey", "Ice", "Insect", "Wood", "Lava", "Moss", "Obsidian", "Oil", "Poison", "Rat", "Salt", "Sand", "Sap", "Serpent", "Slime", "Stone", "Tar", "Thorn", "Vine", "Water", "Wine", "Wood", "Worm"])
